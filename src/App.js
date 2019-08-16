db7da461a603fae141a92487fc1ddbaaed28ff9b
Bon Huang <bonhuan@cisco.com>
2019-08-16 12:59:24 -0700

    init4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
@@ -8,22 +8,22 @@ import SignUp from './components/auth/SignUp'
 import CreateProject from './components/projects/CreateProject'
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path='/'component={Dashboard} />
-            <Route path='/project/:id' component={ProjectDetails} />
-            <Route path='/signin' component={SignIn} />
-            <Route path='/signup' component={SignUp} />
-            <Route path='/create' component={CreateProject} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Navbar/>
+                    <Switch>
+                        <Route exact path='/' component={Dashboard}/>
+                        <Route path='/project/:id' component={ProjectDetails}/>
+                        <Route path='/signin' component={SignIn}/>
+                        <Route path='/signup' component={SignUp}/>
+                        <Route path='/create' component={CreateProject}/>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
