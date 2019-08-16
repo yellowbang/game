59ff82b27a90068e74dc68fc4dd1cdb3728d729c
Bon Huang <bonhuan@cisco.com>
2019-08-16 15:05:44 -0700

    init5

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -6,11 +6,12 @@ import {firestoreConnect} from 'react-redux-firebase'
 import {compose} from 'redux'
 import {Redirect} from 'react-router-dom'
 
-
 class Dashboard extends Component {
     render() {
         const {projects, auth, notifications} = this.props;
-        if (!auth.uid) return <Redirect to='/signin'/>
+        if (!auth.uid) {
+            return <Redirect to='/signin'/>;
+        }
 
         return (
             <div className="dashboard container">
@@ -28,13 +29,12 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
-    // console.log(state);
     return {
         projects: state.firestore.ordered.projects,
         auth: state.firebase.auth,
         notifications: state.firestore.ordered.notifications
     }
-}
+};
 
 export default compose(
     connect(mapStateToProps),
